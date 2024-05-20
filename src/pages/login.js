62d39378c68fc60645a76d081e4a44620078213c
Abdulaziz <abdulaziznejo1281@gmail.com>
2024-05-20 20:05:07 +0300

    first commit

---
+++ src/pages/login.js
@@ -0,0 +1,66 @@
+import './login.css'
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="container-1">
+        <div className="group-10">
+          <div className="login-1">
+            Login
+          </div>
+          <div className="connecting-with-you-every-step-of-the-way">
+            Connecting with You, Every Step of the Way!
+          </div>
+          <div className="group-1">
+            <div className="frame-1">
+              <img className="vector-1" src="assets/vectors/Vector8_x2.svg" />
+            </div>
+            <div className="username">
+              Username
+            </div>
+          </div>
+          <div className="group-2">
+            <div className="frame">
+              <img className="vector" src="assets/vectors/Vector18_x2.svg" />
+            </div>
+            <div className="password">
+              Password
+            </div>
+          </div>
+          <div className="login-2">
+            Login
+          </div>
+          <div className="group-4">
+            <p className="login-with-others">
+              <span className="login-with-others-sub-3"></span><span></span>
+            </p>
+            <img className="subtract" src="assets/vectors/Subtract_x2.svg" />
+          </div>
+          <div className="group-9">
+            <div className="google-1">
+            </div>
+            <p className="continue-with-google">
+              <span className="continue-with-google-sub-0"></span><span></span>
+            </p>
+          </div>
+        </div>
+        <div className="group-8">
+          <div className="group-5">
+            <span className="dont-have-account">
+              Don’t Have account?
+            </span>
+          </div>
+          <div className="container-2">
+            <span className="sign-up">
+              Sign Up
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="rectangle-4">
+        </div>
+      </div>
+    </div>
+  )
+}
